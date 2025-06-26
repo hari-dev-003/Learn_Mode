f18d9829c30d5155b0816c8c03cea10ad4793131
Aditya-NV-06 <adityanv4@gmail.com>
2025-06-26 17:56:37 +0530

    feat: Added the third part component Course Page and the course side bar

---
+++ frontend/src/components/SideBarCourse.jsx
@@ -0,0 +1,17 @@
+function SideBarCourse(){ 
+  return (
+    <div> 
+      <div className="flex flex-col items-center justify-items-normal mt-4 h-full">
+      <h1 className="text-2xl font-bold mb-4">Course Sidebar</h1>
+      <ul> 
+        <li className="m-4 mb-2"><a href="#section1" className="text-blue-500 hover:underline">Section 1</a></li>
+        <li className=" m-4 mb-2"><a href="#section2" className="text-blue-500 hover:underline">Section 2</a></li>
+        <li className="m-4 mb-2"><a href="#section3" className="text-blue-500 hover:underline">Section 3</a></li>
+        <li className="m-4 mb-2"><a href="#section4" className="text-blue-500 hover:underline">Section 4</a></li>
+      </ul>
+    </div>
+  </div>
+  )
+}
+
+export default SideBarCourse;
--- frontend/src/pages/CoursePage.jsx
+++ frontend/src/pages/CoursePage.jsx
@@ -1,8 +1,17 @@
+import SideBarCourse from '../components/SideBarCourse'; 
+
+
 function CoursePage() { 
    return (
-    <>
-       <h1>Course Page</h1> 
-        <p>Welcome to the course page!</p>
+    <> 
+      <div className="grid grid-cols-[25%_70%] h-screen">
+        <SideBarCourse />
+        <div className="flex-1 p-4 border-l border-gray-200">
+            {/* Main content area */}
+            <h2 className="text-2xl font-bold mb-4">Course Content</h2>
+            <p>This is where the course content will be displayed.</p>
+        </div>
+      </div>
     </>
    )
 }
