1317a131a16a33e7bd465d7e18fa70987cac5959
Aditya-NV-06 <adityanv4@gmail.com>
2025-06-26 17:00:12 +0530

    feat: Added the Course page routing and the some changes in the Navbar

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,14 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter,Routes,Route} from 'react-router-dom'
 import HomePage from './pages/HomePage.jsx'
-
+import CoursePage from './pages/CoursePage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
     <Route path="/" element={<HomePage/>}/>
     <Route path="/login" element={<App/>}/>
+    <Route path="/courses" element={<CoursePage/>}/>
     </Routes>
   </BrowserRouter>
   
---
+++ frontend/src/pages/CoursePage.jsx
@@ -0,0 +1,10 @@
+function CoursePage() { 
+   return (
+    <>
+       <h1>Course Page</h1> 
+        <p>Welcome to the course page!</p>
+    </>
+   )
+}
+
+export default CoursePage;
