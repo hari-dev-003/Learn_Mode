80ee9baf03a4ad0e6df038223e2b34e305f59632
Aditya-NV-06 <adityanv4@gmail.com>
2025-06-26 16:17:49 +0530

    chore: Added space for silly and todo for further implementation.

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,14 +1,22 @@
 
 
 function Card({title,description}){ 
+  //TODO: Add functionality to the card component 
+  //TODO: Review the design and layout of the card component
+
+
   return (
      <>
       <div className="text-center max-w-md bg-gray-200 text-black p-4 m-4 rounded-lg shadow-lg">
         <h2 className="text-xl m-4 mt-2 mb-2">{title}</h2>
         <p className="card-description">{description}</p>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-4"> 
+           Learn More
+        </button>
       </div>
     </>
   );
+
 }
 
 export default Card;
