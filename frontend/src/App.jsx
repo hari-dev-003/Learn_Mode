225eab266a0dec80b4074e49f131604491c85712
Hari Dev <hariharidev5@gmail.com>
2025-06-27 10:03:28 +0530

    feat: Add registration functionality with new Register component and page; enhance LoginPage design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
 import CoursePage from './pages/CoursePage';
 import { Routes, Route } from 'react-router-dom';
+import RegisterPage from './pages/RegisterPage';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <Routes>
     <Route path="/" element={<HomePage/>}/>
     <Route path="/login" element={<LoginPage/>}/>
+    <Route path="/register" element={<RegisterPage/>}/>
     <Route path="/courses" element={<CoursePage/>}/>
     </Routes>
     </>
