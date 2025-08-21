777b7d3f224ae3feab4349d0c684ae406f2c7b28
Hari Dev <hariharidev5@gmail.com>
2025-08-21 22:50:09 +0530

    fix: Refactor connectDB function to retrieve MongoDB URL from environment variable

--- backend/db/connect.js
+++ backend/db/connect.js
@@ -5,8 +5,9 @@ dotenv.config({
  path: './.env'
 });
 
-const connectDB = async ( url ) => { 
+const connectDB = async () => { 
   try{ 
+    const url = process.env.MONGO_URL;
     await mongoose.connect(url);
     console.log('MongoDB connected successfully');
   }catch(err)
